27aa1a5cb109951e12ed8fbecb1dbb864ba45395
akeboshi <6316851+akeboshi@users.noreply.github.com>
2025-03-07 15:35:33 +0900

    fix

--- .github/mention/dist/index.js
+++ .github/mention/dist/index.js
@@ -31840,6 +31840,8 @@ async function run() {
       const commentBody = context.payload.comment.body;
       const issueNumber = context.payload.issue.number;
       const repo = context.repo;
+      const commentUrl = context.payload.comment.html_url;
+
 
       // 引数としてとるように変更してもよし
       const members = {
@@ -31850,17 +31852,28 @@ async function run() {
 
       for (const [key, users] of Object.entries(members)) {
         const regex = new RegExp(`\s+to\s+${key}\s+`, "g");
-        if (regex.test(commentBody)) {
-          newMention = users.map(u => `@${u}`).join(" ");
-          break;
-        }
+
+        if (context.eventName === "issue_comment" && context.payload.action === "edited") {
+            const previousBody = context.payload.changes?.body?.from || "";
+  
+            // が新しく mention が追加されたかを確認
+            if (!previousBody.match(regex) && commentBody.match(regex)) {
+              newMention = users.map(u => `@${u}`).join(" ");
+              break;
+            }
+          } 
+          // 新規コメントの場合は通常通りチェック
+          else if (regex.test(commentBody)) {
+            newMention = users.map(u => `@${u}`).join(" ");
+            break;
+          }
       }
 
       if (newMention) {
         await octokit.rest.issues.createComment({
           ...repo,
           issue_number: issueNumber,
-          body: `${newMention} ↑`
+          body: `${newMention}\n[元コメント](${commentUrl})`
         });
       }
     }
